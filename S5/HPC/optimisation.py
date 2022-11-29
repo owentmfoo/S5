8e3f70810236118be143538dbf12ddcf8c5e580c
Owen Foo <owentmfoo@gmail.com>
2022-11-29 21:20:46 +0000

    Fix set_mean and calc_strat: dealing with floating point arithmetic

--- S5/HPC/optimisation.py
+++ S5/HPC/optimisation.py
@@ -1,13 +1,15 @@
 import S5.Tecplot as TP
 import warnings
 import numpy as np
+import math
 
-def calc_strat_new(driver, c, v_bar, x, clip = None):  # TODO: WIP
+
+def calc_strat_new(driver, c, v_bar, x, clip=None):
     '''scale the target velocity to c with mean v_bar and profile driver'''
     n = 8
     if c == 0:
-        v_bar = round(v_bar,n)
-        velout = driver*0+ v_bar
+        v_bar = round(v_bar, n)
+        velout = driver * 0 + v_bar
         return velout
     r = extract_prime(driver, x, n)  # extract variation
     # print(np.percentile(r,95),max(r))
@@ -15,7 +17,8 @@ def calc_strat_new(driver, c, v_bar, x, clip = None):  # TODO: WIP
     vel = v_bar + adj  # adjust the mean value
     return set_mean(vel, v_bar, x, n, clip)
 
-def set_mean(vel, v_bar, x, n, clip = None):
+
+def set_mean(vel, v_bar, x, n, clip=None):
     '''adjust the mean velocity recursively
     :param vel: array of velocity
     :param v_bar: target mean velocity
@@ -23,14 +26,18 @@ def set_mean(vel, v_bar, x, n, clip = None):
     :param n: precision in decimal points
     :param clip: clipping to reduce spikes, None, "kph", or "ms"
     '''
-    velout='spam'
-    vel = np.copy(v_bar + vel - (x.max() - x.min()) / np.trapz(1 / vel, x))
+    velout = 'spam'
+    vel = np.copy(v_bar + vel - np.trapz(vel, x) / (x.max() - x.min()))
     if clip is None:
         velout = vel
     elif clip == "kph":
-        velout = np.clip(vel, 10, 130)
+        clip_max = 130
+        clip_min = 10
+        velout = np.clip(vel, clip_max, clip_min)
     elif clip == "ms":
-        velout = np.copy(np.clip(vel,10/3.6,130/3.6))
+        clip_max = 130 / 3.6
+        clip_min = 10 / 3.6
+        velout = np.copy(np.clip(vel, clip_max, clip_min))
     # elif isinstance(clip,dict): # this was in alpha but not used
     #     if "road" in clip:
     #         roadfile = clip["road"]
@@ -40,19 +47,58 @@ def set_mean(vel, v_bar, x, n, clip = None):
     #
     else:
         velout = vel
+        warnings.warn("clip specifier not valid, no clip is applied.")
 
-    if np.round((x.max() - x.min()) / np.trapz(1 / velout, x), round(n)) == np.round(v_bar, round(n)):
+    if np.isclose(np.trapz(velout, x) / (x.max() - x.min()), v_bar):
         return velout.copy()
+    # else:
+    #     try:
+    #         velout = set_mean(np.copy(vel), v_bar, x, min(n - 0.002, 4),clip) # python recursion limit is 1000
+    #     except RecursionError:
+    #         warnings.warn(f'max recursion reached with v_bar = {v_bar}')
+    #     return velout.copy()
+    velout = _set_mean_clip_optifun(vel, v_bar, x, n, clip=clip)
+    current_delta = np.trapz(velout, x) / (x.max() - x.min()) - v_bar  # detla is positive if the actual is larger
+    # than the target
+    adjust = v_bar - current_delta  # if the actual is larger than target, take some off
+    while not math.isclose(current_delta, 0, abs_tol=1e-12):
+        velout = _set_mean_clip_optifun(vel, v_bar + adjust, x, n, clip=clip)
+        current_delta = np.trapz(velout, x) / (x.max() - x.min()) - v_bar
+        adjust -= current_delta
+
+    # v_bar = v_bar + adjust
+    # vel = np.copy(v_bar + vel - np.trapz(vel, x) / (x.max() - x.min()))
+    # if clip == "kph":
+    #     velout = np.clip(vel, 10, 130)
+    # elif clip == "ms":
+    #     velout = np.copy(np.clip(vel, 10 / 3.6, 130 / 3.6))
+    return velout
+
+
+def _set_mean_clip_optifun(vel, v_bar, x, n, clip=None):
+    '''adjust the mean velocity recursively
+    :param vel: array of velocity
+    :param v_bar: target mean velocity
+    :param x: distnance correcponding to the velocity points
+    :param n: precision in decimal points
+    :param clip: clipping to reduce spikes, None, "kph", or "ms"
+    '''
+    velout = 'spam'
+    vel = np.copy(v_bar + vel - np.trapz(vel, x) / (x.max() - x.min()))
+    if clip is None:
+        velout = vel
+    elif clip == "kph":
+        velout = np.clip(vel, 10, 130)
+    elif clip == "ms":
+        velout = np.copy(np.clip(vel, 10 / 3.6, 130 / 3.6))
     else:
-        try:
-            velout = set_mean(np.copy(vel), v_bar, x, min(n - 0.002, 4),clip) # python recursion limit is 1000
-        except RecursionError:
-            warnings.warn(f'max recursion reached with v_bar = {v_bar}')
-        return velout.copy()
+        velout = vel
+        warnings.warn("clip specifier not valid, no clip is applied.")
+    return velout
 
 
 def extract_prime(vel, x, n):
-    v_prime = vel - np.trapz(vel, x)/(x.max()-x.min())
+    v_prime = vel - np.trapz(vel, x) / (x.max() - x.min())
     assert round(np.trapz(v_prime, x), round(n)) == 0
     return v_prime
     # else:
@@ -63,7 +109,7 @@ def extract_prime(vel, x, n):
     #         return v_prime
 
 
-def calc_strat(driver, c, v_bar, x, clip = None):
+def calc_strat(driver, c, v_bar, x, clip=None):
     return calc_strat_new(driver, c, v_bar, x, clip)
     # r = driver - np.trapz(driver,x)/(x.max()-x.min()) #extract variation
     # adj = r/np.max(r)*c #normalise the magnitude
@@ -73,4 +119,3 @@ def calc_strat(driver, c, v_bar, x, clip = None):
     #     vel = v_bar-(x.max()-x.min())/np.trapz(1/vel, x)+vel
     #     n = max([abs(n-0.001),2])
     # return vel
-
--- S5/tests/test_optimisation.py
+++ S5/tests/test_optimisation.py
@@ -1,60 +1,75 @@
+import random
+
 import pytest
 from S5.HPC.optimisation import *
 import numpy as np
+from pytest import approx
 
 
 def test_set_mean_const():
     x = np.linspace(1, np.pi * 2, 101)
     y = np.array([1] * 101)
     z = set_mean(x, 10, x, 4)
-    assert z.mean() == 10
+    assert z.mean() == approx(10)
 
 
 def test_set_mean_linear():
-    x = np.linspace(0, np.pi * 2, 101)
-    y = x * 1.5
+    x = np.linspace(0, 10, 101)
     z = set_mean(x, 10, x, 4)
-    assert z.mean() == 10
+    assert z.mean() == approx(10)
+    assert np.allclose(np.linspace(5,15,101),z)
 
 
 def test_set_mean():
     x = np.linspace(0, np.pi * 2, 101)
     y = np.sin(x)
     z = set_mean(y, 10, x, 4)
-    assert z.mean() == 10
+    assert z.mean() == approx(10)
+
+def test_set_mean_clip_kph_lin():
+    x = np.linspace(0, 10, 101)
+    y = x.copy()
+    y[random.randint(0,101)] = 100
+    z = set_mean(y, 70, x, 8, 'kph')
+    assert np.trapz(z, x) / (x.max() - x.min()) == approx(70)
+    assert z.max() == approx(130)
 
 
 def test_set_mean_clip_kph():
     x = np.linspace(0, np.pi * 2, 101)
     y = np.sin(x) * 200
-    z = set_mean(y, 10, x, 4, 'kph')
-    assert z.max() == 130
-    assert z.min() == 10
-    assert z.mean() == 10
+    v_bar = random.randint(40,100)
+    z = set_mean(y, v_bar, x, 8, 'kph')
+    assert z.max() == approx(130)
+    assert z.min() == approx(10)
+    assert np.trapz(z, x) / (x.max() - x.min()) == approx(v_bar)
 
 
 def test_set_mean_clip_ms():
     x = np.linspace(0, np.pi * 2, 101)
     y = np.sin(x) * 200
-    z = set_mean(y, 10, x, 4, 'ms')
-    assert z.max() == 130 / 3.6
-    assert z.min() == 10 / 3.6
-    assert z.mean() == 10
+    v_bar = random.randint(10, 27)
+    z = set_mean(y, v_bar, x, 8, 'ms')
+    assert z.max() == approx(130 / 3.6)
+    assert z.min() == approx(10 / 3.6)
+    assert np.trapz(z, x) / (x.max() - x.min()) == approx(v_bar)
 
 
 def test_calc_strat_zero():
     x = np.linspace(0, np.pi * 2, 101)
     y = np.sin(x)
     z = calc_strat(y, 0, 10, x)
-    assert z.mean() == 10
-    assert z.max() == 10
-    assert z.min() == 10
+    assert z.mean() == approx(10)
+    assert z.max() == approx(10)
+    assert z.min() == approx(10)
 
 
 def test_calc_strat():
     x = np.linspace(0, np.pi * 2, 101)
     y = np.sin(x)
     z = calc_strat(y, 1, 10, x)
-    assert z.mean() == 10
-    assert z.max() == 11
-    assert z.min() == 9
+    assert z.mean() == approx(10)
+    assert np.percentile(z, 68) == approx(11)
+    assert np.percentile(z, 32) == approx(9)
+
+#TODO: add tests with real data
