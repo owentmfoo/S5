4308ac878c43da62a5a71d5114d9784a233ff013
owentmfoo <owentmfoo@gmail.com>
2023-11-11 23:44:09 +0000

    Format scripts from WSC23, docstrings

--- S5/visualisation/__init__.py
+++ S5/visualisation/__init__.py
@@ -1,158 +1,207 @@
 """
-Post simulation analysis scripts
-Script to plot the car through time and space overlaid on top of the weather contours.
-This version have the distance on the x-axis
-WIP
+Functions to visualise Strategy related data.
 """
 import os
+from typing import Union, Tuple
 
 import matplotlib
-import matplotlib.dates as dates
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import dates
 
 import S5.Tecplot as TP
 
-matplotlib.rcParams.update({
-    "pgf.texsystem": "pdflatex",
-    'font.family': 'serif',
-    'text.usetex': False,
-    'pgf.rcfonts': False,
-    'font.size': 8,
-})
+matplotlib.rcParams.update(
+    {
+        "pgf.texsystem": "pdflatex",
+        "font.family": "serif",
+        "text.usetex": False,
+        "pgf.rcfonts": False,
+        "font.size": 8,
+    }
+)
 
-plt.rcParams.update({'font.serif': 'Times New Roman'})
+plt.rcParams.update({"font.serif": "Times New Roman"})
 
 
-def plot_contors(contour_parameter, weather, VarName='', cmap='viridis',
-                 ax=None,
-                 **kwargs):
+def plot_contor(
+        contour_parameter: str,
+        weather: TP.SSWeather,
+        display_name: str = "",
+        cmap="viridis",
+        ax=None,
+        **kwargs,
+) -> Tuple[plt.Figure, plt.Axes]:
+    """Plots a variable from the weather file as a contour map.
+
+    The plot will have space in the x-axis and time in the y-axis.
+
+    Args:
+        contour_parameter: Column name of the parameter to plot.
+        weather: Weather file containing the data.
+        display_name: Name of the variable to label the figure.
+        cmap: Colormap to use, passed onto matplotlib.axes.Axes.pcolormesh,
+            default to be 'viridis'.
+        ax: Optional, if present then this will be axes to plot on.
+        **kwargs: Additional arguments to pass along to
+            matplotlib.axes.Axes.pcolormesh.
+
+    Returns:
+        A tuple of (figure, axes)
+
+    Examples:
+        >>> weather = TP.SSWeather("C:/path/to/Weather.dat")
+        >>> weather.add_timestamp('20231023')
+        >>> plot_contor(
+        >>>     "DirectSun (W/m2)",
+        >>>     weather,
+        >>>     display_name="Direct Irradiation (W/m²)",
+        >>>     cmap="inferno",
+        >>>     ax=ax[0],
+        >>> )
+    """
     cont = weather.data.loc[:, [contour_parameter]].to_numpy()
-    Dist = np.unique(weather.data.loc[:, ['Distance (km)']].to_numpy())
-    DateTime = np.unique(weather.data.loc[:, ['DateTime']].to_numpy())
+    dist = np.unique(weather.data.loc[:, ["Distance (km)"]].to_numpy())
+    date_time = np.unique(weather.data.loc[:, ["DateTime"]].to_numpy())
 
-    DateTimenum, Dist = np.meshgrid(dates.date2num(DateTime), Dist)
-    plotvel = np.reshape(cont, np.shape(Dist))
+    date_timenum, dist = np.meshgrid(dates.date2num(date_time), dist)
+    plotvel = np.reshape(cont, np.shape(dist))
     if ax is None:
         fig, ax = plt.subplots()
     else:
         fig = ax.get_figure()
 
     # plot the contour
-    c = ax.pcolormesh(Dist, DateTimenum, plotvel, cmap=cmap, **kwargs)
+    c = ax.pcolormesh(dist, date_timenum, plotvel, cmap=cmap, **kwargs)
     cbar = fig.colorbar(c, ax=ax)
-    cbar.set_label(VarName, rotation=270, labelpad=10)
+    cbar.set_label(display_name, rotation=270, labelpad=10)
     yticks = dates.num2date(ax.get_yticks())
     yticks = [out.strftime("%d-%m-%Y\n%H:%M ") for out in yticks]
     ax.set_yticklabels(yticks)
 
     # Format the figure
-    ax.set_title(VarName)
+    ax.set_title(display_name)
     ax.set_xlim([0, 3030])
     ax.set_xlabel("Distance (km)")
     ax.set_ylabel("Time")
     return fig, ax
 
 
-def save_fig(fig: plt.figure, outname: str):
-    fig.set_size_inches(6, 8)
-    fig.subplots_adjust(top=0.9, bottom=0.12, left=0.1, right=1.05, hspace=0,
-                        wspace=0)
-    fig.set_dpi(150)
-    fig.show()
+def plot_trace(
+        hist: TP.SSHistory, ax: plt.Axes = None, set_limit=True, **kwargs
+) -> Tuple[plt.Figure, plt.Axes]:
+    """Plots the location of the car on a time-distance graph.
 
-    outname = outname.rstrip(".pdf").rstrip(".png")
-    fig.savefig(f'{outname}.png')
+    This can be used in conjunction with plot_contor to show how the car is
+    positioned in relation to clouds or period of high wind.
+    Requires the columns 'DateTime' and 'Distance(km)' present in the DataFrame.
+
+    Args:
+        hist: History file to plot the trace.
+        ax: Optional, axes to plot the trace on.
+        set_limit: Set the axes limit only to the period where position data is
+            present
+        **kwargs: Additional arguments to pass along to matplotlib.pyplot.plot.
+
+    Returns:
+        A tuple of (figure, axes)
+
+    Raises:
+        KeyError: If DateTime is not present in
+
+    Examples:
+        >>> history = TP.SSHistory("C:/path/to/History.dat")
+        >>> weather = get_weather(history.filename)
+        >>> history.add_timestamp('20231023')
+        >>> weather.add_timestamp('20231023')
+        >>> plot_contor(
+        >>>     "DirectSun (W/m2)",
+        >>>     weather,
+        >>>     display_name="Direct Irradiation (W/m²)",
+        >>>     cmap="inferno",
+        >>>     ax=ax[0],
+        >>> )
+        >>> plot_trace(hist=history, ax=ax[0], lw=2)
+    """
+    if "DateTime" not in hist.data.columns:
+        raise KeyError(
+            "DateTime column not present in History file, consider "
+            "using history.add_timestamp to add the datetime colum."
+        )
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.get_figure()
+    ax.plot(hist.data["Distance(km)"], hist.data["DateTime"], **kwargs)
+    if set_limit:
+        ax.set_ylim(
+            dates.date2num(hist.data["DateTime"].min()) - 1 / 24,
+            dates.date2num(hist.data["DateTime"].max()) + 1 / 24,
+        )
+    return fig, ax
 
 
-def plot_trace(hist, ax=None, **kwargs):
-    ax.plot(hist.data['Distance(km)'], hist.data['DateTime'], **kwargs)
-    ax.set_ylim([dates.date2num(hist.data['DateTime'].min()) - 1 / 24,
-                 dates.date2num(hist.data['DateTime'].max()) + 1 / 24])
+def plot_airspeed(
+        history: TP.SSWeather, **kwargs
+) -> plt.Figure:
+    """
 
+    Args:
+        history: History file, with DataTime as index
+        **kwargs: Additional arguments to pass along to matplotlib.pyplot.plot.
 
-def plot_airspeed(history_path):
-    HISTORY = TP.SSHistory(history_path)
-    HISTORY.add_timestamp(startday=START_DATE)
-    hist = HISTORY.data
-    hist = hist.set_index('DateTime')
-    hist['Day'] = hist['DDHHMMSS'] // 1000000
-    hist['km500'] = hist['Distance(km)'] // 500
-    filter = hist['CarVel(m/s)'] == 0
-    hist = hist[filter == False]
-    # Get the average wind speed per day
-    hist['AirSpeed(m/s)'] = hist['CarVel(m/s)'] + hist['HeadWind(m/s)']
-    fig, ax = plt.subplots(nrows=hist['Day'].nunique(), ncols=2)
+    Returns:
+        Figure containing all the plots
+    """
+    if "marker" not in kwargs:
+        kwargs["marker"] = "."
+
+    hist = history.data
+    hist = hist.set_index("DateTime")
+    hist["Day"] = hist["DDHHMMSS"] // 1000000
+    hist = hist[hist["CarVel(m/s)"] != 0]
+    hist["AirSpeed(m/s)"] = hist["CarVel(m/s)"] + hist["HeadWind(m/s)"]
+
+    fig, ax = plt.subplots(nrows=hist["Day"].nunique(), ncols=2)
     fig.set_size_inches(8.3, 11.7)
-    # fig.set_dpi(150)
-    fig.suptitle(HISTORY.filename)
+    fig.suptitle(history.filename)
     fig.tight_layout(rect=(0.11, 0.02, 1, 0.98))
-    for i, day in enumerate(hist['Day'].unique()):
-        hist[hist['Day'] == day][['AirSpeed(m/s)']].plot(subplots=True,
-                                                         marker='.', ax=ax[
-                i, 0])  # TODO: allow this to be override by some way?
-        hist[hist['Day'] == day][['HeadWind(m/s)']].plot(subplots=True,
-                                                         marker='.',
-                                                         ax=ax[i, 1])
+
+    for i, day in enumerate(hist["Day"].unique()):
+        hist[hist["Day"] == day][["AirSpeed(m/s)"]].plot(
+            subplots=True, ax=ax[i, 0], **kwargs
+        )
+        hist[hist["Day"] == day][["HeadWind(m/s)"]].plot(
+            subplots=True, ax=ax[i, 1], **kwargs
+        )
         ax[i, 0].set_ylim([19, 30])  # TODO: better way to set limits.
-        ax[i, 0].set_title(f'{os.path.basename(HISTORY.filename)} Day {day}')
-    plt.show()
+        ax[i, 0].set_title(f"{os.path.basename(history.filename)} Day {day}")
     return fig
 
 
-def get_weather(history_path):
-    HISTORY = TP.SSHistory(history_path)
-    HISTORY.add_timestamp(startday=START_DATE)
-    Control = TP.DSWinput(
-        os.path.join(os.path.dirname(HISTORY.filename), "SolarSim.in")
+def get_weather(history_path: Union[str, os.PathLike]) -> TP.SSWeather:
+    """Get the corresponding weather file for the given history file.
+
+    The Weather file is found by looking at SolarSim.in at the same folder as
+    the history file.
+
+    Args:
+        history_path: Path to the history file.
+
+    Returns:
+        Weather file object.
+
+    Examples:
+        >>> history = TP.SSHistory("C:/path/to/History.dat")
+        >>> weather = get_weather(history.filename)
+    """
+    control = TP.DSWinput(
+        os.path.join(os.path.dirname(history_path), "SolarSim.in")
     )
-    Weather = TP.SSWeather(
-        os.path.join(os.path.dirname(HISTORY.filename),
-                     Control.get_value("WeatherFile").strip('"'))
+    weather = TP.SSWeather(
+        os.path.join(
+            os.path.dirname(history_path),
+            control.get_value("WeatherFile").strip('"'),
+        )
     )
-    Weather.add_timestamp(startday=START_DATE)
-    return Weather
-
-
-if __name__ == '__main__':
-    START_DATE = '20230928'
-    Weather = TP.SSWeather(
-        r'E:\WSC23\MidRace_VelSweep_Day0\Weather-latest-2023-09-28-Day2.dat')
-    Weather.add_timestamp(startday=START_DATE)
-
-    HISTORY = TP.SSHistory(r'E:\WSC23\MidRace_VelSweep_Day0\History_63.dat')
-    HISTORY.add_timestamp(startday=START_DATE)
-    cm = plt.get_cmap('Set1')
-
-    fig, ax = plt.subplots(nrows=2, sharex=True)
-    fig.set_size_inches(6, 8)
-    plot_contors('DirectSun (W/m2)', Weather,
-                 VarName='Direct Irradiation (W/m²)', cmap='inferno', ax=ax[0]
-                 )  # outname="directContor_Dist")
-    plot_trace(hist=HISTORY, ax=ax[0], c=cm.colors[0], lw=2)
-    plot_contors('DiffuseSun (W/m2)', Weather,
-                 VarName='Diffuse Irradiation (W/m²)',
-                 cmap='inferno',
-                 ax=ax[1]
-                 )  # outname="diffuseContor_Dist")
-    plot_trace(hist=HISTORY, ax=ax[1], c=cm.colors[0], lw=2)
-    fig, ax = plot_contors('WindVel (m/s)', Weather,
-                           VarName='Wind Velocity (m/s)',
-                           cmap='viridis',
-                           )  # outname="windContor_Dist")
-    plot_trace(hist=HISTORY, ax=ax, c=cm.colors[0], lw=2)
-    fig, ax = plot_contors('WindDir (deg)', Weather, VarName='Wind Dir (deg)',
-                           cmap='twilight',
-                           )  # outname="windDirContor_Dist")
-    plot_trace(hist=HISTORY, ax=ax, c=cm.colors[0], lw=2)
-    fig, ax = plot_contors('AirTemp (degC)', Weather,
-                           VarName='Air Temperature (°C)',
-                           cmap='coolwarm',
-                           )  # outname="tempContor_Dist")
-    plot_trace(hist=HISTORY, ax=ax, c=cm.colors[0], lw=2)
-    fig, ax = plot_contors('AirPress (Pa)', Weather,
-                           VarName='Air Pressure (Pa)',
-                           cmap='coolwarm',
-                           )  # outname="pressContor_Dist")
-    plot_trace(hist=HISTORY, ax=ax, c=cm.colors[0], lw=2)
-    plt.show()
+    return weather
